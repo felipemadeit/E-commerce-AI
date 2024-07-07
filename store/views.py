ca988d4e2a96091e60359854281d1f175092c893
felipemadeit <felipemadeit@gmail.com>
2024-07-06 22:07:50 -0500

    Update requirements.txt

--- store/views.py
+++ store/views.py
@@ -1,8 +1,6 @@
 import json
 from pyexpat.errors import messages
 from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
